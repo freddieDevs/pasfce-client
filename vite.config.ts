931e9d6a4cb12a2b89c94a9c42d2a39cfae0d1e4
freddieDevs <133527315+freddieDevs@users.noreply.github.com>
2023-09-24 04:12:56 +0300

    changed the api url

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://doubtful-foal-beanie.cyclic.cloud', // nest apiurl
+        target: 'https://long-jade-eagle-wear.cyclic.cloud/api', // nest apiurl
         changeOrigin: true
       },
     },
